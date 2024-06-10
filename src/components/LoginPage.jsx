18f5b35f81b3d1a0071972a4d8335983bc316bf2
Kabo Khudunyane <88645384+KaboKhudunyane@users.noreply.github.com>
2024-06-10 05:51:34 +0200

    final login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '../css/LoginPage.css';
 
 function LoginPage() {
 
@@ -10,23 +11,31 @@ function LoginPage() {
 
     return (
         <div className="login-page">
+
             <div className="login-container">
+
                 <div className="login-form">
+
                     <h2>Login into your account</h2>
+
                     <form onSubmit={handleSubmit}>
                         <div className="input-group">
                             <label htmlFor="email">Email address</label>
                             <input type="email" id="email" />
                         </div>
+
                         <div className="input-group">
                             <label htmlFor="password">Password</label>
                             <input type="password" id="password" />
                         </div>
+
                         <div className="forgot-password">
                             <a href="#">Forgot password?</a>
                         </div>
+
                         <button type="submit" className="login-btn">Login now</button>
                     </form>
+
                     <div className="signup-option">
                         <span>OR</span>
                         <button type="button" className="signup-btn">Signup now</button>
